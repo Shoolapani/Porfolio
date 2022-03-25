784da0406402b7fd9208e79073d588fe6b01bbff
Shoolapani <satyamkrjha040@gmail.com>
2022-03-25 11:09:11 +0530

    25 March 2022

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Mahadev")
+// console.log("Mahadev")
+
+// To hide cross
+document.querySelector(".cross").style.display = "none";
+
+// Click Listener
+document.querySelector(".hamburger").addEventListener("click", () => {
+  // will toggle sidebar
+  document.querySelector(".sidebar").classList.toggle("sidebarGo");
+  if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
+    document.querySelector(".ham").style.display = "inline";
+    document.querySelector(".cross").style.display = "none";
+  } else {
+    document.querySelector(".ham").style.display = "none";
+    setTimeout(() => {
+      document.querySelector(".cross").style.display = "inline";
+    },300);
+  }
+});
