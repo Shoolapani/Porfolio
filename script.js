6fe508e553298c32debd636ad92687222530e24c
Shoolapani <satyamkrjha040@gmail.com>
2022-03-26 15:39:25 +0530

    26 March 2022

--- script.js
+++ script.js
@@ -14,6 +14,18 @@ document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".ham").style.display = "none";
     setTimeout(() => {
       document.querySelector(".cross").style.display = "inline";
-    },300);
+    }, 300);
   }
 });
+
+function sendEmail(params) {
+  var temParams = {
+    from_name:document.getElementById("clientName").value,
+    to_name:(document.getElementById("clientEmail").value),
+    message:document.getElementById("message").value
+  }
+  emailjs.send('service_uw7463w','template_fpz079d',temParams)
+  .then(function(res){
+    console.log("Success",res.status);
+  })
+}
